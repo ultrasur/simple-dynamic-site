df8a53eef130498e6e8c10eae5dc935af053c74a
ultrasur <166250895+ultrasur@users.noreply.github.com>
2024-11-23 11:12:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function changeMessage() {
+    const messageElement = document.getElementById("dynamicMessage");
+    messageElement.textContent = "You clicked the button! The message has changed.";
+}
